f833e62ced158ea1da5123a457a54b0734f4c57b
mayuridongre <36354118+mayuridongre@users.noreply.github.com>
2018-05-15 15:26:33 -0400

    Website test

---
+++ dokuwiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+mayuri:$1$435mDNze$aYjmcuUPyT64rNPmyi0lB0:Mayuri Dongre:dongremayuri.22@gmail.com:admin,user
